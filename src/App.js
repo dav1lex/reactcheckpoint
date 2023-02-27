4a362f1de0ffbae66066eaf516e9a7ff9cbb0638
dav1lex <zedxurl@gmail.com>
2023-02-27 17:03:21 +0100

    ff

---
+++ src/App.js
@@ -0,0 +1,52 @@
+import {useState} from "react";
+import {nanoid} from "nanoid";
+
+function App() {
+    const [student, setstudent] = useState({
+        id: nanoid(),
+        name: '',
+        classCount: 0,
+        inClass: false,
+    })
+
+    const [val, setval] = useState('')
+
+    function inputlistener(e) {
+        e.preventDefault()
+        setval(e.target.value)
+
+    }
+
+    function joinclass() {
+        setstudent((prevState) => ({
+            ...prevState,
+            inClass: !student.inClass,
+            classCount: prevState.classCount++
+        }))
+
+    }
+
+    function changename() {
+      setstudent((prevState)=>({
+          ...prevState,
+          name:val
+      }))
+    }
+
+    console.log(student)
+    return (
+        <>
+            <div>
+                {`${student.name} ---- ${student.classCount}---- ${student.inClass}`}
+                <form>
+                    <input onChange={inputlistener} value={val}/>
+                    <button type='button' onClick={changename}>chane student name</button>
+
+                </form>
+                <button onClick={joinclass}>{student.inClass ? 'Leave the class' : 'Join the class'}</button>
+            </div>
+        </>
+    );
+}
+
+export default App;
